397e38a3ceba5efe3f52e579e7919dd30daa48f7
Stephen <stephenleesummers@gmail.com>
2020-03-21 14:07:41 -0600

    Add MoveHistory class

--- Game.java
+++ Game.java
@@ -5,27 +5,10 @@
  */
 public class Game
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
     /**
      * Constructor for objects of class Game
      */
     public Game()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
     }
 }
---
+++ MoveHistory.java
@@ -0,0 +1,13 @@
+
+/**
+ * Keeps log of moves made by both players.
+ */
+public class MoveHistory
+{
+    /**
+     * Constructor for objects of class MoveHistory
+     */
+    public MoveHistory()
+    {
+    }
+}
