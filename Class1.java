978cb21731b5783596c6a14e23a8a016d046918a
Jeremy <jeremysummers492@gmail.com>
2020-03-20 18:23:02 -0700

    Added middle print statement

--- Class1.java
+++ Class1.java
@@ -3,7 +3,7 @@ public class Class1
   public static void main(String args[])
   {
     System.out.println("This is a test.");
-    
+    System.out.println("Stephen stinks at Civ VI");
     System.out.println("Stephen is a noob");
   }
 }
