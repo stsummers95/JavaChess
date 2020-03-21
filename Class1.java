7a0daf01159ed4276caafab83d2fbc14972f0f3f
Stephen <stephenleesummers@gmail.com>
2020-03-20 19:22:56 -0600

    Add statement for merge conflict

--- Class1.java
+++ Class1.java
@@ -3,7 +3,7 @@ public class Class1
   public static void main(String args[])
   {
     System.out.println("This is a test.");
-    
+    System.out.println("This is a merge conflict.");
     System.out.println("Stephen is a noob");
   }
 }
